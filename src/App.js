fa391474b5945db53e17899d2af6ed938b929171
Ram2871 <ramchandra@r2logics.com>
2023-04-16 15:34:10 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import AddEvent from "./components/EventList/AddEvent";
+
+function App() {
+  return (
+    <div className="App">
+        
+        <AddEvent/>
+      
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/InputField/InputField.js
@@ -0,0 +1,29 @@
+import React from 'react'
+import './InputField.css'
+const InputField = (props) => {
+  const togglePasswd = function() {
+    const passwd = document.getElementById(props.id);
+    const togglePasswordIcon = document.querySelector('#togglePassword')
+    // console.log(passwd);
+    const type = passwd.getAttribute('type') === 'password' ? 'text' : 'password';
+    passwd.setAttribute('type', type);
+    const toggleEye = togglePasswordIcon.getAttribute('class') === 'fa-solid fa-eye eyeLogo' ? 'fa-solid fa-eye-slash eyeLogo' : 'fa-solid fa-eye eyeLogo';
+    togglePasswordIcon.setAttribute('class', toggleEye);
+    // this.classList.toggle('fa-eye-slash');
+  }
+  return (
+
+       <div className="input-container ic2">
+            <input id={props.id} className="input" ref={props.fRef} onChange={props.onChangeField} type={props.type} placeholder=" " required />
+            {props.type==='password'?
+            <i style={{color:'red'}} id="togglePassword" onClick={togglePasswd} className="fa-solid fa-eye eyeLogo"></i>
+            :<span></span>}
+            <div className={`cut ${props.type==='password'?'':'cut-short'}`} style={{width:props.cutWidth}}></div>
+            <label htmlFor={props.label} className="placeholder">{props.label}</label>
+           
+        </div>
+   
+  )
+}
+
+export default InputField
